3478a0236b2fdeb6a7a00ce180ca88dd8e4d8353
A <apfela1@hawkmail.newpaltz.edu>
2018-10-18 16:54:03 -0400

    sharedwithNames, formatted responses, total admin access

--- server/session/controller.js
+++ server/session/controller.js
@@ -13,7 +13,7 @@ app.get("/", function(req, res){
 
 //create user w/ name
 app.post('/users', (req, res) => {
-    const user = new User(req.body.name, session.users.length, req.body.weight);
+    const user = new User(req.body.name, session.users.length, req.body.weight, req.body.height);
     session.users.push(user);
     res.send(user);
 })
@@ -23,7 +23,10 @@ app.post('/users', (req, res) => {
 app.get('/users/:id', (req, res) => {
     var playerId = req.header("playerId");
     if(session.users[req.params.id].sharedwith.includes(String(playerId))){
-        res.send("{Name: " + session.users[req.params.id].name + "}, {Exercise: " + session.users[req.params.id].exercise + "}, {Diet: " + session.users[req.params.id].diet + "}");
+        res.send(session.users[req.params.id].name + " has completed the following exercises: " + session.users[req.params.id].exercise + 
+        "\n" + session.users[req.params.id].name + " has eaten the following meals: " + session.users[req.params.id].diet + 
+        "\n" + session.users[req.params.id].name + " has previously weighed: " + session.users[req.params.id].pastweight +
+        "\n" + session.users[req.params.id].name + " currently weighs " + session.users[req.params.id].weight + ", has a height of " + session.users[req.params.id].height + " inches, and a bmi of " + session.users[req.params.id].bmi);
     }
     else{
         res.send("This user has not granted you access");
@@ -50,11 +53,11 @@ app.post('/exercise', (req, res) => {
 })
 
 //display only exercise based on id #
-//only the user can view raw data
+//only the user can view individual data
 app.get("/users/:id/exercise", function(req, res){
     var playerId = req.header("playerId");
-    if(session.users[req.params.id].id == playerId){
-        res.send(session.users[req.params.id].exercise);    
+    if((session.users[req.params.id].id == playerId) || (playerId == -1)){
+        res.send("You have completed the following exercises: " + session.users[req.params.id].exercise);    
     }
     else{
         res.send("Only the user can view their individual data");
@@ -69,11 +72,11 @@ app.post('/diet', (req, res) => {
 })
 
 //display only diet based on id #
-//only the user can view raw data
+//only the user can view individual data
 app.get("/users/:id/diet", function(req, res){
     var playerId = req.header("playerId");
-    if(session.users[req.params.id].id == playerId){
-        res.send(session.users[req.params.id].diet);    
+    if((session.users[req.params.id].id == playerId) || (playerId == -1)){
+        res.send("You have eaten the following meals: " + session.users[req.params.id].diet);    
     }
     else{
         res.send("Only the user can view their individual data");
@@ -81,24 +84,36 @@ app.get("/users/:id/diet", function(req, res){
 })
 
 //add weight based on id #
-//only the user can view raw data
 app.post('/weight', (req, res) => {
     var playerId = req.header("playerId");
-    session.addFood(req.body.weight, playerId);
+    session.addWeight(req.body.weight, playerId);
     res.send("weight added");
 })
 
 //display only weight based on id #
-//only the user can view raw data
+//only the user can view individual data
 app.get("/users/:id/weight", function(req, res){
     var playerId = req.header("playerId");
-    if(session.users[req.params.id].id == playerId){
-        res.send(session.users[req.params.id].pastweight);    
+    if((session.users[req.params.id].id == playerId) || (playerId == -1)){
+        res.send("Your current weight: " + session.users[req.params.id].weight + 
+        "\n" + "Your previous weights: " + session.users[req.params.id].pastweight);    
     }
     else{
         res.send("Only the user can view their individual data");
     }
 })
+//display only bmi based on id #
+//only the user can view individual data
+app.get("/users/:id/bmi", function(req, res){
+    var playerId = req.header("playerId");
+    if((session.users[req.params.id].id == playerId) || (playerId == -1)){
+        res.send("Your current bmi: " + session.users[req.params.id].bmi);    
+    }
+    else{
+        res.send("Only the user can view their individual data");
+    }
+})
+
 
 //add viewing access to specified users
 app.post('/sharedwith', (req, res) => {
@@ -107,10 +122,17 @@ app.post('/sharedwith', (req, res) => {
     res.send(session.users[req.body.id].name + " can now view your exercise and diet");
 })
 
-/*
-//view ids shared with certain id
-app.get("/sharedwith/:id", function(req, res){
-    res.send(session.users[req.params.id].sharedwith);
-})*/
+
+//view names of people who can view your fitness information
+//only user can view individual data
+app.get("/users/:id/sharedwith", function(req, res){
+    var playerId = req.header("playerId");
+    if((session.users[req.params.id].id == playerId) || (playerId == -1)){
+        res.send("Users who can view your fitness information: " + session.users[req.params.id].sharedwithName);
+    }
+    else{
+        res.send("Only the user can view their individual data");
+    }
+})
 
 module.exports = app;
--- server/session/model.js
+++ server/session/model.js
@@ -4,37 +4,52 @@ class Session{
         this.users = [];
     }
 
+    //adds meal to diet array
     addFood(food, id){
         this.users[id].diet.push(food);
     }
 
+    //adds workout to exercise array
     addExercise(work, id){
         this.users[id].exercise.push(work);
     }
 
+    //adds weight to past weight array, assigns weight to the users current weight, updates the bmi
     addWeight(weight, id){
-        this.users[id].pastweight.push(weight);
+        this.users[id].pastweight.push(this.users[id].weight);
         this.users[id].weight = weight;
+        this.users[id].bmi = ((weight*703) / (Math.pow(this.users[id].height, 2))).toFixed(1);
     }
 
+    //returns length of users array
     getUserNumber(){
         return this.users.length;
     }
 
+    //gives access to user with allowedId to view fitness information of user with id
     addAccess(id, allowedId){
         this.users[id].sharedwith.push(allowedId);
+        this.users[id].sharedwithName.push(this.users[allowedId].name);
     }
+
 
 }
 
 class User{
-    constructor(name, id, weight) {
+    constructor(name, id, weight, height) {
 
         this.id = id;
         this.name = name;
+        //weight in pounds
         this.weight = weight;
+        //height in inches
+        this.height = height;
+        //bmi is cut off after 1 decimal place
+        this.bmi = ((this.weight*703) / (Math.pow(this.height, 2))).toFixed(1);
         this.pastweight = [];
-        this.sharedwith = [String(id)];
+        //admin access (id = -1)
+        this.sharedwith = [String(id), "-1"];
+        this.sharedwithName = [];
         this.exercise = [];
         this.diet = [];
     }
