5dd2ef633d212784df8aa6d8642d1897fcc8c0e8
A <apfela1@hawkmail.newpaltz.edu>
2018-11-20 08:50:21 -0500

    Revert "installed bootstrap and sass loader, moved html to homepage"
    This reverts commit 80fd0717af3d883e04664b5d4d85dd6942132749.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,8 @@
 <template>
-<div>
-<div class="jumbotron">
-    <div class="container text-center">
-        <h1 class="display-3">Fitness App</h1>
-        <p class="lead">
-          Follow your progress and stay on track using the simplest and most effective fitness app around. Our services include diet tracking, exercise tracking, and more to make sure you reach your goals!
-        </p>
-        <a class="btn btn-primary" href="#" role="button">Login</a></br>
-        <a class="btn btn-primary" href="#" role="button">Sign up</a>
-    </div>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
-  <div class="container">
-  <div class="row">
-    <div class="col">
-      <figure>
-        <img class="img-fluid rounded-circle" src="https://www.verywellfit.com/thmb/BebPKxIRNfz-QYECQfu9CaW2DSU=/1920x1080/filters:fill(FFDB5D,1)/Illo_Exercise-5a626d274e4f7d00373dead6.png" alt="Exercise">
-        <figcaption>After each workout, the user can type the description of his/her workout (e.g. 10 reps and 3 sets of…) into a note (and add pictures if desired). The user can also input the time for how long he/she worked out. The user can then tag this note with certain tags for which body part he/she worked out that day (e.g. legs, arms, core, etc.) or what type of training (e.g. cardio, strength training, flexibility, etc.) or rest (because rest days are important).</figcaption>
-      </figure>
-    </div>
-    <div class="col">
-      <figure>
-        <img class="img-fluid rounded-circle" src="http://designatprinting.com/wp-content/uploads/healthy-food-clipart-25-18-clip-art.jpg" alt="Diet">
-        <figcaption>
-            The user can input exactly what they're eating into a note so he/she can reference it in the future. The user can tag this with certain nutritional tags (e.g. protein, carbs, etc.) or more specifically by food groups (e.g. fruit, vegetables, meat, etc.). The purpose of the diet tracking is not meant to track every calorie but to enable the user to have a general idea what he/she is eating on a consistent basis. Calorie counting and tracking exact nutritional information is not necessary for most people. 
-        </figcaption>
-      </figure>
-    </div>
-    <div class="col">
-      <figure>
-        <img class="img-fluid rounded-circle" src="https://www.eastvista.com/wp-content/uploads/2016/11/small-steps.png" alt="Progress">
-        <figcaption>
-            The user can update his/her weight at anytime but he/she will be prompted to do so weekly in order to track progress. In addition, BMI will be tracked based on height and current weight.
-            The user can view progress numerically or graphically.            
-        </figcaption>
-      </figure>
-    </div>
-  </div>
-  </div>
-</div>
 </template>
 
 <script>
